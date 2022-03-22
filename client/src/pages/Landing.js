61cd2427237bf5b15240cc198581219220bb36a6
Ryo Fujiwara <jjcproject@icloud.com>
2022-03-22 23:50:37 +0900

    2nd day complete with tutorial of MERN

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,21 +1,64 @@
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
+import styled from 'styled-components'
+
 const Landing = () => {
   return (
-   <main>
+   <Wrapper>
       <nav>
         <img src={logo} alt="yes" className="logo" />
       </nav>
       <div className="container page">
         {/* info */}
         <div className="info">
-          <h1>Track My Activity</h1>
+          <h1><span>Track</span> My Activity</h1>
           <p>I'm baby godard actually four dollar toast stumptown, normcore pitchfork coloring book offal gluten-free. Unicorn skateboard next level food truck flexitarian kitsch distillery kickstarter iPhone.</p>
           <button className='btn btn-hero'>Login/Register</button>
         </div>
-        <img src={main} alt="activity tracker" className='img-main-img'/>
+        <img src={main} alt="activity tracker" className='main-img'/>
       </div>
-   </main>
+   </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display:flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display:grid;
+    align-items: center;
+    margin-top:-3rem;
+  }
+
+  h1 {
+    font-weight: 700;
+    span {
+      color:var(--primary-500);
+    }
+  }
+  p {
+    color:var(--grey-600);
+  }
+  .main-img {
+    display:none;
+  }
+  @media (min-width:992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`
+
+
 export default Landing
