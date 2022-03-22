0dc623bc0e734c8282a92eaba456f6f321e30f3b
Ryo Fujiwara <jjcproject@icloud.com>
2022-03-22 23:30:57 +0900

    second day on doing the tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import Landing from './pages/Landing'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: blue;
+  color: #fff;
+  font-size:1rem;
+`
+
+const ButtonSecond = styled.button`
+  background: red;
+  color: #fff;
+  font-size:1rem;
+`
 
 function App() {
   return (
     <div>
+      <Button>Click Here</Button>
+      <ButtonSecond>Click Here</ButtonSecond>
       <h1>Search</h1>
     <Landing/>  
     </div>
