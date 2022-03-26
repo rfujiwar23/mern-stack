2db3e3d140904f1013a4ff4ab903672a63c2f7fe
Ryo Fujiwara <jjcproject@icloud.com>
2022-03-26 14:20:08 +0900

    yes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import Landing from './pages/Landing'
-import styled from 'styled-components'
-
-
 
 function App() {
   return (
     <div>
+    {/* <h1>jobify</h1> */}
     <Landing/>  
     </div>
   );
