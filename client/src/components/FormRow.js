7d942976f43ab0fc5c5cf6ae6448dc8a1ed13026
Ryo Fujiwara <jjcproject@icloud.com>
2022-03-28 23:57:20 +0900

    yes

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -1,8 +1,16 @@
-const FormRow = () => {
+const FormRow = ({type,name,value,handleChange,labelText}) => {
   return (
     <div className='form-row'>
-        <label htmlFor='name' className='form-label'>Name</label>
-        <input type="text" value={values.name} name="name" onChange={handleChnge} className='form-input'/>
+        <label htmlFor='name' className='form-label'>
+            Name
+        </label>
+        <input 
+            type="text" 
+            value={values.name} 
+            name="name" 
+            onChange={handleChnge} 
+            className='form-input'
+        />
     </div>
   )
 }
