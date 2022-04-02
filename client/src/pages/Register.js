78ff392571f121373dfa77b7cd4868d78f8ecc5a
Ryo Fujiwara <jjcproject@icloud.com>
2022-04-02 22:45:46 +0900

    yes

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
 
-  const {isLoading,showAlert} = useAppContext()
+  const {isLoading, showAlert, displayAlert} = useAppContext()
   // console.log(state)
 
   const toggleMember = () => {
@@ -22,12 +22,17 @@ const Register = () => {
   };
 
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({...values, [e.target.name]: e.target.value})
   };
 
   const onSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target);
+    e.preventDefault()
+    const {name,email,password,isMember} = values
+    if(!email || !password || (!isMember && !name)){
+      displayAlert()
+      return
+    }
+    console.log(values)
   };
 
   return (
